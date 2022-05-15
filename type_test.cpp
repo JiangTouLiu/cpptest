5bd8c011cd6c5d65dac8866d7abcc33706446937
JiangTouLiu <99812428+JiangTouLiu@users.noreply.github.com>
2022-05-15 18:06:46 +0800

    Update type_test.cpp

--- type_test.cpp
+++ type_test.cpp
@@ -3,6 +3,52 @@
 #include <iostream>
 using namespace std;
 
+#define _CRT_SECURE_NO_WARNINGS 1
+#define PI 3.14
+#include <iostream>
+using namespace std;
+
+//定义枚举变量，枚举所有可能的形状
+enum shape
+{
+	CIRCLE,
+	RECTANGLE,
+	SQUARE
+};
+
+
+//计算圆形的面积
+void AreaOfCircle()
+{
+	unsigned int r = 0;
+	cout << "请输入圆的半径：" << endl;
+	cin >> r;
+	cout << "半径为" << r << "的圆的面积是：" << PI * r * r << endl;
+}
+
+//计算长方形的面积
+void AreaOfRectangle()
+{
+	unsigned int length = 0;
+	unsigned int width = 0;
+	cout << "请输入长方形的长：" << endl;
+	cin >> length;
+	cout << "请输入长方形的宽：" << endl;
+	cin >> width;
+	cout << "长方形的面积是：" << length * width << endl;
+}
+
+//计算正方形的面积
+void AreaOfSquare()
+{
+	unsigned int side_of_length = 0;
+	cout << "请输入正方形的边长： " << endl;
+	cin >> side_of_length;
+	cout << "边长为" << side_of_length << "的正方形的面积是：" << side_of_length * side_of_length << endl;
+}
+
+
+
 int main()
 {
 	//使用sizeof操作符，计算常见类型的大小
@@ -35,5 +81,31 @@ int main()
 	cin >> y;
 	z = x > y ? (x - y) : (y - x);
 	cout << "the diferent of x and y is:" << z << endl;
+	
+	
+	//计算不同形状的面积
+	int s = 0;
+	cout << "plese select shape:" << endl;
+	cout << "---------------   0: circle  ------------------------" << endl;
+	cout << "---------------   1: rectangle  ------------------------" << endl;
+	cout << "---------------   2: square  ------------------------" << endl;
+	cin >> s;
+	switch (s)
+	{
+		case CIRCLE:
+			AreaOfCircle();
+			break;
+		case RECTANGLE:
+			AreaOfRectangle();
+			break;
+		case SQUARE:
+			AreaOfSquare();
+			break;
+		default:
+			cout << "选择错误，请重新选择" << endl;
+			break;
+	}
+	
+	
 	return 0;
 }
