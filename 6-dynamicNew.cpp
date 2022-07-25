69863e9b88b324f26850de73bbe21de5c167c908
JiangTouLiu <99812428+JiangTouLiu@users.noreply.github.com>
2022-07-25 23:19:18 +0800

    Update 6-dynamicNew.cpp

--- 6-dynamicNew.cpp
+++ 6-dynamicNew.cpp
@@ -21,6 +21,23 @@ int main()
 		delete[] p;
 		p = NULL;
 	}
+	
+	Point* p = new Point[2];
+	if (p == NULL)
+	{
+		cout << "开辟空间失败" << endl;
+	}
+	else
+	{
+		cout << "p1:" << p[0].getX() << "," << p[0].getY() << endl;
+		cout << "p2:" << p[1].getX() << "," << p[1].getY() << endl;
+		p[0].move(10, 20);
+		p[1].move(20, 30);
+		cout << "p1:" << p[0].getX() << "," << p[0].getY() << endl;
+		cout << "p2:" << p[1].getX() << "," << p[1].getY() << endl;
+		delete[] p;
+		p = NULL;
+	}
 
 	return 0;
 }
