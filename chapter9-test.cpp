d7909f684afc5b5b406b41648fba4422699c4704
JiangTouLiu <99812428+JiangTouLiu@users.noreply.github.com>
2022-09-06 21:20:30 +0800

    Update chapter9-test.cpp

--- chapter9-test.cpp
+++ chapter9-test.cpp
@@ -1,147 +1,236 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <iostream>
+#include <cassert>
 using namespace std;
+
+//函数模板
+template <typename T>
+T abs1(T t)
+{
+	return t >0? t:-t;
+}
 
 class Point
 {
 public:
-	Point();
-	Point(int a, int b):x(a), y(b) {};
-	Point& operator ++();
-	Point operator ++(int);
-	Point& operator --();
-	Point operator --(int);
-	void show();
+	Point() 
+	{
+		x = 0;
+		y = 0;
+	}
+	Point(int a, int b) :x(a), y(b) {};
 	friend ostream& operator <<(ostream& out, Point& p);
 private:
 	int x;
 	int y;
 };
 
-Point::Point()
+template <class T>
+void show(T t)
 {
-	x = 0;
-	y = 0;
-};
+	cout << t << endl;
+}
 
-Point& Point::operator ++()
-{
-	++this->x;
-	++this->y;
-	return *this;
-};
-
-Point Point::operator ++(int)
-{
-	Point tem = *this;
-	++*this;
-	return tem;
-};
-
-Point& Point::operator --()
-{
-	--this->x;
-	--this->y;
-	return *this;
-};
-
-Point Point::operator --(int)
-{
-	Point tem = *this;
-	--* this;
-	return tem;
-};
-
-ostream& operator << (ostream& out, Point& p)
+ostream& operator <<(ostream& out, Point& p)
 {
 	out << "(" << p.x << "," << p.y << ")";
 	return out;
 };
 
-class Vehicle
+class ComplexNum
 {
 public:
-	virtual void run();
-	virtual void stop();
+	ComplexNum(int r, int v) :real(r), vir(v) {};
+	friend ostream& operator <<(ostream& out, ComplexNum& c);
 private:
-	string style;
+	int real;
+	int vir;
 };
 
-void Vehicle::run()
+ostream& operator <<(ostream& out, ComplexNum& p)
 {
-	cout << "Vehicle is running" << endl;
+	out << "(" << p.real << "," << p.vir << ")";
+	return out;
 };
 
-void Vehicle::stop()
-{
-	cout << "Vehicle is stopping" << endl;
-};
+//类模板
 
-class Bicycle :virtual public Vehicle
+template <class T>
+class Store
 {
 public:
-	virtual void run();
-	virtual void stop();
+	Store();
+	void putElement(const T& t);
+	T& getElement();
 private:
-	int height;
+	T item;
+	bool haveValue;
 };
 
-void Bicycle::run()
+template <class T>
+Store <T>::Store()
 {
-	cout << "Bicycle is running" << endl;
-};
+	haveValue = false;
+}
 
-void Bicycle::stop()
+template <class T>
+void Store<T>::putElement(const T& t)
 {
-	cout << "Bicycle is stopping" << endl;
-};
+	item = t;
+	haveValue = true;
+}
 
-class Motocycle :virtual public Vehicle
+template <class T>
+T& Store<T>::getElement()
+{
+	if (haveValue)
+	{
+		return item;
+	}
+	else
+	{
+		cout << "there is nothing" << endl;
+	}
+	
+}
+
+template <class T>
+class Array
 {
 public:
-	virtual void run();
-	virtual void stop();
+	Array(int sz);
+	Array();
+	Array(const Array<T>& a);
+	~Array();
+	Array<T>& operator =(const Array<T>& a);
+	T& operator [](int i);
+	int getSize();
+	void resetSize(int sz);
+	operator T *();
 private:
-	int height;
+	T* arr;
+	int size;
 };
 
-void Motocycle::run()
+template <class T>
+Array<T>::Array(int sz)
 {
-	cout << "Motocycle is running" << endl;
-};
+	assert(sz > 0);
+	size = sz;
+	arr = new T[size];
+}
 
-void Motocycle::stop()
+template <class T>
+Array<T>::Array()
 {
-	cout << "Motocycle is stopping" << endl;
-};
+	size = 50;
+	arr = new T[size];
+}
 
-void start(Vehicle* v)
+template <class T>
+Array<T>::Array(const Array<T>& a)
 {
-	v->run();
-};
+	size = a.size;
+	arr = new T[size];
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = a.arr[i];
+	}
+}
 
+template <class T>
+Array<T>::~Array()
+{
+	delete [] arr;
+}
 
+template <class T>
+T& Array<T>::operator [](int i)
+{
+	assert(i >= 0 && i < size);
+	return arr[i];
+}
+
+template <class T>
+Array<T>& Array<T>:: operator =(const Array<T>& a)
+{
+	if (&a == this)
+	{
+		return;
+	}
+	if (a.size != size)
+	{
+		size = a.size;
+		delete[] arr;
+		arr = new T[size];
+	}
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = a.arr[i];
+	}
+	return *this;
+}
+
+template <class T>
+Array<T>::operator T *()
+{
+	return arr;
+}
+
+template <class T>
+void Array<T>::resetSize(int sz)
+{
+	assert(sz > 0);
+	if (sz == size)
+	{
+		return;
+	}
+	T* newarr = new T[sz];
+	int n = (sz < size) ? sz : size;
+	for (int i = 0; i < n; i++)
+	{
+		newarr[i] = arr[i];
+	}
+	delete[] arr;
+	arr = newarr;
+	newarr = NULL;
+	size = sz;
+}
+
+template <class T>
+int Array <T>::getSize()
+{
+	return size;
+}
 
 
 int main()
 {
-	Vehicle v;
-	Bicycle b;
-	Motocycle m;
-	Vehicle* vp = &v;
-	vp->run();
-	vp->stop();
-	vp = &b;
-	vp->run();
-	vp->stop();
-	vp = &m;
-	vp->run();
-	vp->stop();
-	/*Point p1;
-	cout << p1++ << endl;
-	cout << p1 << endl;
-	cout << ++p1 << endl;
-	cout << p1-- << endl;
-	cout << --p1 << endl;*/
+	/*Point p(10, 10);
+	ComplexNum c(20, 20);
+	show(p);
+	show(c);*/
+	/*Store <int> s1;
+	s1.putElement(5);
+	cout << s1.getElement() << endl;
+	Store <double> s2;
+	s2.putElement(3.14);
+	cout << s2.getElement() << endl;
+	Store <Point> s3;
+	Point p(10, 20);
+	s3.putElement(p);
+	cout << s3.getElement() << endl;
+	Store <Point> s4;
+	cout << s4.getElement() <<endl;*/
+
+	Array <int> ai;
+	for (int i = 0; i < ai.getSize(); i++)
+	{
+		ai[i] = i;
+	}
+	for (int i = 0; i < ai.getSize(); i++)
+	{
+		cout << ai[i] << endl;
+	}
 	return 0;
 }
